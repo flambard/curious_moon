f183a99f34514e4a13cb86d111ef5229d3fd85e8
Markus Flambard <markus@flambard.se>
2019-04-07 00:39:53 +0200

    Added lookup table for teams

--- scripts/normalize.sql
+++ scripts/normalize.sql
@@ -22,3 +22,13 @@ select
   import.master_plan.title,
   import.master_plan.description
 from import.master_plan;
+
+
+
+drop table if exists teams;
+select distinct(team)
+as description
+into teams
+from import.master_plan;
+alter table teams
+add id serial primary key;
